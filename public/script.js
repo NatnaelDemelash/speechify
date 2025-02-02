c485182646941402058cfe836dabdd63ad9b2271
NatnaelDemelash <natidemelash074@gmail.com>
2025-02-02 11:48:48 +0300

    load available voices and loading them as an option

--- public/script.js
+++ public/script.js
@@ -2,6 +2,23 @@ const voiceSelect = document.querySelector('#voiceSelect');
 const playButton = document.querySelector('#playButton');
 const textInput = document.querySelector('textarea');
 
+// Load all voices
+let voices = [];
+
+function loadVoices() {
+  voices = speechSynthesis.getVoices();
+  voiceSelect.innerHTML = voices
+    .map(
+      (voice, index) =>
+        `<option value="${index}">${voice.name} (${voice.lang})</option>`
+    )
+    .join('');
+}
+
+// Trigger loadVoices when voices are loaded
+speechSynthesis.onvoiceschanged = loadVoices;
+loadVoices();
+
 // Play TTS
 playButton.addEventListener('click', () => {
   const utterance = new SpeechSynthesisUtterance(textInput.value);
